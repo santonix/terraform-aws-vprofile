3a985fcc8dabac37dd6a2e658bd8359065a5d932
santonix <santonix@hotmail.it>
2024-09-22 11:17:04 +0200

    updates

--- vpc.tf
+++ vpc.tf
@@ -4,8 +4,8 @@ module "vpc" {
    name = var.vpc_name
    cidr = var.vpc_cidr_block
    azs = [var.zone1, var.zone2, var.zone3]
-   private_subnets = [var.private_subnet1_cidr_block, var.private_subnet2_cidr_block,var.private_subnet3_cidr_block]
-   public_subnets = [var.public_subnet1_cidr_block.var.public_subnet2_cidr_block, var.public_subnet3_cidr_block]
+   private_subnets = [var.private_subnet1_cidr_block, var.private_subnet2_cidr_block, var.private_subnet3_cidr_block]
+   public_subnets = [var.public_subnet1_cidr_block, var.public_subnet2_cidr_block, var.public_subnet3_cidr_block]
 
    enable_nat_gateway = true
    single_nat_gateway = true
