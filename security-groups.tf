47a22a59ba435f832232ecf2cd6299a199b0e1fc
santonix <santonix@hotmail.it>
2024-09-22 12:26:32 +0200

    fix syntax errors

--- security-groups.tf
+++ security-groups.tf
@@ -3,19 +3,23 @@ resource "aws_security_group" "vprofile-bean-elb-sg"{
     description = "security group for bean-elb"
     vpc_id = module.vpc.vpc_id
 
-    ingress = {
+    ingress =  [ 
+      {
         from_port = 80
         to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
-    }
+      }
+    ]  
 
-    egress = {
+    egress = [
+      {
         from_port = 0
         to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
-    }
+      }
+    ]  
 
 }
 
@@ -25,19 +29,24 @@ resource "aws_security_group" "vprofile-bastion-sg" {
     description = "security group for bastion host ec2 instance"
     vpc_id = module.vpc.vpc_id
 
-    egress = {
+    egress = [
+      {
         from_port = 0
         to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
-    }
+      }
+    ]  
 
-    ingress = {
+    ingress = [
+    
+      {
         from_port = 22
         to_port = 22
         protocol = "tcp"
         cidr_blocks = [var.MYIP]
-    }
+      }
+    ]  
 
 }
 
@@ -46,19 +55,22 @@ resource "aws_security_group" "vprofile-prod-sg" {
     description = "sg for beanstalk instances"
     vpc_id = module.vpc.vpc_id
 
-    egress = {
+    egress = [
+      {
         from_port = 0
         to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress = {
+      }
+    ]
+    ingress = [
+      {
         from_port = 22
         to_port = 22
         protocol = tcp
         security_groups = [aws_security_group.vprofile-bastion-sg.id]
-    }
+      }
+    ]  
 }
 
 resource "aws_security_group" "vprofile-backend-sg" {
@@ -66,19 +78,23 @@ resource "aws_security_group" "vprofile-backend-sg" {
     description = "sg for RDS, active mq, elastic cached"
     vpc_id = module.vpc.vpc_id
 
-    egress = {
+    egress = [
+      {
         from_port = 0
         to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
-    }
+      }
+    ]  
 
-    ingress = {
+    ingress = [
+      {
         from_port = 0
         to_port = 0
         protocol = "tcp"
         security_groups = [aws_security_group.vprofile-prod-sg.id]
-    }
+      }
+    ]  
 }
 
 resource "aws_security_group_rule" "sec_group_allow_itself" {
