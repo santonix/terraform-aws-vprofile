5b7698224f65aee810ec3d4c104f7fec6e640364
santonix <santonix@hotmail.it>
2024-09-23 21:46:55 +0200

    updates

--- backend-services.tf
+++ backend-services.tf
@@ -1,32 +1,16 @@
-resource "aws_db_subnet_group" "vprofile-rds-subnet-group" {
-    name = "main"
-    subnet_ids = [ module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2] ]
-    tags = {
-      Name = "subnet-group-for-RDS"
-    }
-}
-
-resource "aws_elasticache_subnet_group" "vprofile-elasticache-subnet-group" {
-    name = "vprofile-elasticache-subnet-group"
-    subnet_ids = [ module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2] ]
-    tags = {
-      Name = "subnet-group-for-elasticache"
-    }
-}
-
 resource "aws_db_instance" "default" {
   allocated_storage    = 20
-  storage_type = "gp2"
+  storage_type         = "gp2"
   db_name              = var.dbname
   engine               = "mysql"
   engine_version       = "8.0.35"
   instance_class       = "db.t3.micro"
   username             = var.dbuser
   password             = var.dbpass
-  parameter_group_name = "default.mysql5.6"
+  parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
-  publicly_accessible = "false"
-  multi_az = "false"
+  publicly_accessible  = false
+  multi_az             = false
   db_subnet_group_name = aws_db_subnet_group.vprofile-rds-subnet-group.name
   vpc_security_group_ids = [ aws_security_group.vprofile-backend-sg.id ]
 }
@@ -38,20 +22,17 @@ resource "aws_elasticache_cluster" "vprofile-cache" {
   num_cache_nodes      = 1
   parameter_group_name = "default.memcached1.6"
   port                 = 11211
-  security_group_ids = [ "aws_security_group.vprofile-backend-sg" ]
-  subnet_group_name = aws_elasticache_subnet_group.vprofile-elasticache-subnet-group.name
+  security_group_ids   = [ aws_security_group.vprofile-backend-sg.id ]
+  subnet_group_name    = aws_elasticache_subnet_group.vprofile-elasticache-subnet-group.name
 }
 
 resource "aws_mq_broker" "vprofile-rmq" {
-  broker_name = "vprofile-rmq"
-
-  
-
-  engine_type        = "ActiveMQ"
-  engine_version     = "5.17.6"
-  host_instance_type = "mq.t2.micro"
-  security_groups   = ["aws_security_group.vprofile-backend-sg"]
-  subnet_ids = [module.vpc.private_subnets[0]]
+  broker_name          = "vprofile-rmq"
+  engine_type          = "ActiveMQ"
+  engine_version       = "5.17.6"
+  host_instance_type   = "mq.t2.micro"
+  security_groups      = [ aws_security_group.vprofile-backend-sg.id ]
+  subnet_ids           = [module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
 
   user {
     username = var.rmquser
